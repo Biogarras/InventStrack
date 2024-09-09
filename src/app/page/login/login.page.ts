f03da6f9b6ec83e1fdd1b08d642250b5d3a9c4a5
juliobienbueno <julioignaciob@gmail.com>
2024-09-08 22:51:02 -0300

    arreglo de login

---
+++ src/app/page/login/login.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/services/autenticacion/autenticacion.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  username: string = "";
+  password: string = "";
+
+
+  constructor(private _authService: AutenticacionService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+
+  login(username: string, password: string){
+    if(this._authService.autentificacion(username, password)){
+      console.info("Usuario Existe")
+      this.router.navigate(['dashboard'], {
+        state: {
+          usuario: username
+        }
+      })
+    }else{
+      console.error("Usuario No existe")
+    }
+  }
+
+}
