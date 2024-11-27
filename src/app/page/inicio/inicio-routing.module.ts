e8e012d6a777a9ef6e7973e6b523bc2b51c53bbd
Biogarras <reyes.enre@gmail.com>
2024-11-27 11:06:46 -0300

    correccion routing

--- src/app/page/inicio/inicio-routing.module.ts
+++ src/app/page/inicio/inicio-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { InicioPage } from './inicio.page';
-import { ListaTiendasPage } from 'InventStrack/src/app/page/lista-tiendas/lista-tiendas.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: ListaTiendasPage
-  },
- 
+    component: InicioPage
+  }
 ];
 
 @NgModule({
