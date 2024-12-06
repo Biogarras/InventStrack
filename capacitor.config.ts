e89cff4da70ff4ec4b0499df25449b1011527fcb
Biogarras <reyes.enre@gmail.com>
2024-12-06 00:51:28 -0300

    preuab 12?

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,16 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'InventStrack',
   webDir: 'www',
+  plugins: {
+    'BarcodeScanner': {
+      android: {
+        // Aquí puedes agregar configuraciones específicas de Android si es necesario
+      },
+      ios: {
+        // Y configuraciones de iOS si las necesitas
+      },
+    },
+  },
   
 };
 
